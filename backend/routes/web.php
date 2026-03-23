88a2657c567dad9b5a8514c93950cd31fbb7bad7
David Njoroge <davidnjorosalme606@gmail.com>
2026-03-23 14:21:30 +0300

    new laravel

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,26 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\PropertyController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::middleware('auth')->group(function() {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard'); // make sure it has a name!
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-});    
-
-    Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
-    Route::get('/properties/create', [PropertyController::class, 'create'])->name('properties.create');
-    Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
-});
-
-require __DIR__.'/auth.php';
